7db94c72a6f27be245ecdbb20a46671ced3af483
acl1414 <alain.clinquart@gmail.com>
2020-01-17 16:24:56 +0100

    implementation fenetre

--- src/com/clinquart/main/Component.java
+++ src/com/clinquart/main/Component.java
@@ -1,5 +1,66 @@
 package com.clinquart.main;
 
-public class Component {
+import org.lwjgl.LWJGLException;
+import org.lwjgl.Sys;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
 
+public class Component {
+	
+	public boolean running = false;
+	
+	public static int scale = 3; //echelle
+	public static int width =720 / scale ;
+	public static int height =480 / scale;
+	public static String title = "TITRE";
+	
+	DisplayMode mode = new DisplayMode(width * scale, height *scale);
+
+	public Component() {
+		try {
+			Display.setDisplayMode(mode);
+			Display.setResizable(true);
+			Display.setFullscreen(false);
+			Display.setTitle(title);
+			Display.create();
+		} 
+		catch (LWJGLException e) {
+		
+			e.printStackTrace();
+		}
+	}
+	
+	public void start() {
+	running = true;
+	loop();	
+	}
+	
+	public void stop() {
+		Display.destroy();
+		System.exit(0);
+	}
+	
+	public void exit() {
+		running = false;
+	}
+	
+	public void loop() {
+	
+		while (running) 
+		{
+			if (Display.isCloseRequested()) 
+			{
+				stop();
+			}
+			Display.update();
+			System.out.println(running);
+		}
+		
+		exit();
+	}
+	
+	public static void main(String[] args) {
+		Component main = new Component();
+		main.start();
+	}
 }
